b47777d713889d4459db3635cd3d231a78eed612
gengsa.wang <gengsa.wang@heavengifts.com>
2018-12-06 10:17:56 +0800

    init

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index()
+    {
+        $cart = [
+            'items'=> [
+                'goods1',
+                'goods2',
+                'goods3',
+                'goods4',
+                'goods5',
+                'goods6',
+                'goods7',
+            ]
+
+        ];
+        return view('index', ['cart' => $cart]);
+    }
+}
